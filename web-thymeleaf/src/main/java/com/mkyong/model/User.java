1486c8596cf1543733f0c35ed62a77ac98ccc764
Mohamed Lotfy <mohamed1.lotfy.ext@orange.com>
2020-09-14 13:09:15 +0200

    initial commit

---
+++ web-thymeleaf/src/main/java/com/mkyong/model/User.java
@@ -0,0 +1,4 @@
+package com.mkyong.model;
+
+public class User {
+}
---
+++ web-thymeleaf/src/main/resources/static/js/form.js
@@ -0,0 +1,118 @@
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
+}
+console.log("HELLO");
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    sendPostRequest();
+//    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
+console.log("Document Get ready ...");
+//$(document).ready(function(){
+//console.log("Document Get ready ... ");
+//    $("form").on("submit", function(event){
+//    console.log("Submitting ...")
+//        event.preventDefault();
+//
+////        var formValues= $(this).serialize();
+//
+//    });
+//});
+function sendPostRequest(){
+        var data  = getFormData($("#regForm"));
+        console.log(data, "before sending")
+//        $.post(
+//        "http://localhost:8080/users",
+//         data,
+//         dataType: "json",
+//        ).done(function(data){
+//
+//        });
+
+   $.ajax({
+            url: 'http://localhost:8080/users',
+            type: 'post',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                        console.log(data);
+            },
+            data: JSON.stringify(data)
+        });
+
+}
+
+function getFormData($form){
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+        indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+}
